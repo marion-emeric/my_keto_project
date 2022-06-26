b73cae7aaad7f558ecf4ceb3d18abf200de10557
Marion Emeric Bourgogne <marion@360medics.com>
2022-06-26 12:07:07 +0200

    comment RegistrationController

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,15 +11,15 @@ class RegistrationController extends AbstractController
     public function index(UserPasswordHasherInterface $passwordHasher)
     {
         // ... e.g. get the user data from a registration form
-        $user = new User(...);
-        $plaintextPassword = ...;
-
-        // hash the password (based on the security.yaml config for the $user class)
-        $hashedPassword = $passwordHasher->hashPassword(
-            $user,
-            $plaintextPassword
-        );
-        $user->setPassword($hashedPassword);
+//        $user = new User();
+//        $plaintextPassword = ...;
+//
+//        // hash the password (based on the security.yaml config for the $user class)
+//        $hashedPassword = $passwordHasher->hashPassword(
+//            $user,
+//            $plaintextPassword
+//        );
+//        $user->setPassword($hashedPassword);
 
         // ...
     }
